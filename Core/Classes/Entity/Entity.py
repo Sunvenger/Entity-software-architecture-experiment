63d9368621bf4442aefb72dfabec39c305acb9b8
Sunvenger <sunvenger@gmail.com>
2018-03-26 08:30:28 +0200

    Created Git repository

--- Core/Classes/Entity/Entity.py
+++ Core/Classes/Entity/Entity.py
@@ -1,10 +1,22 @@
 # Entity class
-# -Entita može byť zároveň prepojením entít ktorá na ne priamo odkazuje
-# -Entita má operatívny paralelný priestor (Task) ktorý načíta vstupné dáta ktoré sprostredkováva Prepojenie.
-# V tomto momente sú dáta prenesené do paralelného priestoru entity
+# Dizajn 1:
+# -(Entita) može byť zároveň prepojením entít ktorá na ne priamo odkazuje
+# -(Entita) má operatívny paralelný priestor (Task) ktorý načíta vstupné dáta ktoré sprostredkováva
+#  (komunikačný kanál.)
 #
+# -(Entita) može s ostatnými entitami mat otvorený lubovolný počet komunikačných kanálov
+# -(Komunikačný kanál) definuje sposob komunikácie entít napríklad založený na posielaní správ ostatným entitám
+#  alebo na zdielanie nejakého stavu medzi entitami.
+#  Pričom zdielanie stavu funguje tak, že pri každej zmene zdielaneho atribútu je poslaná správa všetkým entitám
+#  ktorým je atribút zdielaný
 #
-# Prepojenia samotné môžu mať definované ľubovolné operácie ktoré môžu manipulovať s obomi prepojenými entitami
+# -alebo -> entity A B majú povolené obojstranne komunikovať.
+#  entita A pošle entite B správu o stave jedného jej atribútu Teplota = 99°.
+#  entita B nevedela o tom, že entita B má atribút Teplota a do záznamu o entite A založí nový atribút a priradí
+#  mu hodnotu 99°
+
+
+#
 #
 
 
